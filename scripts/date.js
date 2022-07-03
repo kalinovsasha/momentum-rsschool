496329d53598204cc52a03180e074dfd8416189a
Kalinov Alexander <kalinovsasha@yandex.ru>
2022-07-03 14:55:40 +0300

    feat: add time

---
+++ scripts/date.js
@@ -0,0 +1,13 @@
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+
+
+function showTime() {
+  const date = new Date();
+  const options = {month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'};
+  const currentDate = date.toLocaleDateString('ru-RU', options);
+  timeEl.innerHTML=(date.toLocaleTimeString());
+  dateEl.innerHTML=(currentDate)
+  setTimeout(showTime, 1000);
+}
+showTime();
