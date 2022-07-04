ebfc74c4340d18e2ddddc70f1db46e8c9c5c1719
Kalinov Alexander <kalinovsasha@yandex.ru>
2022-07-04 12:51:58 +0300

    feat:add quotes

---
+++ scripts/quotes.js
@@ -0,0 +1,31 @@
+quotes = {
+  changeQuotebtn:document.querySelector(".change-quote"),
+  quoteEl:document.querySelector(".quote"),
+  authorEl:document.querySelector(".author"),
+  dbQuotes:[],
+  
+  getQuotes() {
+    const quotes = 'db.json';
+    fetch(quotes)
+      .then(res => res.json())
+      .then(data => { 
+        this.dbQuotes=data;
+        console.log(this.dbQuotes);
+        this.viewQuotes();
+      });
+  },
+
+  viewQuotes(){
+    const quote = this.dbQuotes[Math.floor(Math.random()*this.dbQuotes.length)];
+    this.quoteEl.innerHTML=quote.text;
+    this.authorEl.innerHTML=quote.author;
+    return quote
+  },
+
+  init(){
+    this.getQuotes();
+    this.changeQuotebtn.onclick=()=>this.viewQuotes();
+  }
+}
+
+quotes.init();
